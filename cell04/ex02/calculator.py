5dac36252a5741c7059f40b5a5a97acf16a3e090
earnn10 <warisara20082551@gmail.com>
2025-03-11 12:21:14 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,9 @@
+num1 = float(input("Give me the first number: "))
+num2 = float(input("Give me the second number: "))
+
+print("Thank you!")
+
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} / {num2} = {num1 / num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
