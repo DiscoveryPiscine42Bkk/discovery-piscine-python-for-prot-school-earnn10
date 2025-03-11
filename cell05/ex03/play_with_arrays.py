13ce51598c6c9bbf0f9c06ab7cbca0d06db723ec
earnn10 <warisara20082551@gmail.com>
2025-03-11 14:13:49 +0700

    Add files via upload

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,16 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+order = [24, 10, 11, 50] # ลำดับที่ต้องการ
+
+for num in original_array:
+    if num > 5 and num + 2 in order:
+        if num + 2 not in new_array:
+            new_array.append(num + 2)
+
+final_array = []
+for num in order:
+    if num in new_array:
+        final_array.append(num)
+
+print(original_array, "$")
+print(final_array, "$")
